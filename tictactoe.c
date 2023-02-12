5ecc653a1fad148f3e220995ab1fb0a68f0f0356
Daniel Kristensen <50834782+dankri0274@users.noreply.github.com>
2023-02-12 02:47:33 +0100

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -1,28 +1,52 @@
-// * File name: tic-tac-toe.c
-
 #include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
 
 #define LINE printf("+---+---+---+\n");
+#define clearScreen printf("\e[1;1H\e[2J"); // Clear screen
 
-int main(void) {
+int main() {
 	int turn;
-	int player, winner, choice, row, col, line, whosturn;
+	int player, winner, choice, row, col, line;
 
-	char board[3][3] = {{'1', '2', '3'},
-						{'4', '5', '6'},
-						{'7', '8', '9'}};
+	player = 0;
+	winner = 2;
+	choice = 0;
+	row    = 0;
+	col    = 0;
+	line   = 0;
 
-	player	= 0;
-	winner	= 0;
-	choice	= 0;
-	row		= 0;
-	col		= 0;
-	line	= 0;
+	bool gameOver = false;
 
-	printf("\nTic-Tac-Toe");
-	// Draw the board
-	for (turn = 0; turn < 9 && winner == 0; turn++) {
-		printf("\n\n");
+	char playerNames[2][16];
+	char playerSymbols[2];
+
+	char board[3][3] = {
+		{'1', '2', '3'},
+		{'4', '5', '6'},
+		{'7', '8', '9'}
+	};
+	
+	for (int i = 0; i < 2; i++) {
+		clearScreen;
+		printf("Player %d, what is your name: ", i + 1);
+		scanf(" %15[^\n]%*c", playerNames[i]);
+	}
+	
+	for (int i = 0; i < 2; i++) {
+		clearScreen;
+		printf("%s, choose symbol to use: ", playerNames[i]);
+		scanf(" %c", &playerSymbols[i]);
+	}
+
+	if (playerSymbols[0] == playerSymbols[1]) {
+		printf("\nBoth players has chosen the same character!\nPlease choose another character.\n\n");
+		return 1;
+	}
+
+	clearScreen;
+
+	for (turn = 0; turn < 9 && !gameOver; turn++) {
 		LINE
 		printf("| %c | %c | %c |\n", board[0][0], board[0][1], board[0][2]);
 		LINE
@@ -31,56 +55,46 @@ int main(void) {
 		printf("| %c | %c | %c |\n", board[2][0], board[2][1], board[2][2]);
 		LINE
 
-		player = (turn % 2) + 1; //* Select player
-		whosturn = (player == 1) ? 'X' : 'O';
+		if (turn % 2 == 0) { player = 0; }
+		else               { player = 1; }
 
 		do {
-			printf("\nEnter your go, player %c: ", whosturn);
+			printf("%s, it\'s your turn: ", playerNames[player]);
 			scanf("%d", &choice);
 
 			row = --choice / 3;
 			col = choice % 3;
 		}
 		while (choice < 0 || choice > 9 || board[row][col] > '9');
-		
-		board[row][col] = whosturn; // Insert player symbol
 
-		// Check for a winner
-		if ((board[0][0] == board[1][1] &&
-			 board[0][0] == board[2][2]) ||
-			(board[0][2] == board[1][1] &&
-			 board[0][2] == board[2][0])) {
+		board[row][col] = playerSymbols[player]; // Insert player symbol
+
+		if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
+			(board[0][2] == board[1][1] && board[0][2] == board[2][0])) {
 				winner = player;
+				gameOver = true;
 			}
 		else {
-			for(line = 0; line <= 2; line++) {
-				if ((board[line][0] == board[line][1] &&
-					 board[line][0] == board[line][2]) ||
-					(board[0][line] == board[1][line] &&
-					 board[0][line] == board[2][line])) {
+			for (line = 0; line <= 2; line++) {
+				if ((board[line][0] == board[line][1] && board[line][0] == board[line][2]) ||
+					(board[0][line] == board[1][line] && board[0][line] == board[2][line])) {
 						winner = player;
-				} // End if check line
-			} // End for loop check line
-		} // End if else check for winner
-	} // End of for loop
+						gameOver = true;
+				}
 
-	printf("\nGame Finished!");
+				clearScreen
+			}
+		}
+	}
 
-	printf("\n\n");
-	LINE
-	printf("| %c | %c | %c |\n", board[0][0], board[0][1], board[0][2]);
-	LINE
-	printf("| %c | %c | %c |\n", board[1][0], board[1][1], board[1][2]);
-	LINE
-	printf("| %c | %c | %c |\n", board[2][0], board[2][1], board[2][2]);
-	LINE
-
-	if (winner == 0) {
-		printf("\nHow boring, it\'s a draw\n");
+	if (winner != 0 && winner != 1) {
+		printf("How boring, it\'s a draw\n");
 	}
 	else {
-		printf("\nPLAYER %c IS THE WINNER!\n");
+		printf("PLAYER \"%s\" WINS!\n", playerNames[winner]);
 	}
+
+	Sleep(5000);
 
 	return 0;
 }
